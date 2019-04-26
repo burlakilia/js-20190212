72ca7733e43fccf24a7db0dfc1ca0e9b8a4d3c55
Alexey Khabarov <alexey.khabarov@mail.ru>
2019-04-26 22:38:41 +0300

    add onclick event

--- blocks/email-helper/email-helper.js
+++ blocks/email-helper/email-helper.js
@@ -22,42 +22,21 @@ export class EmailHelper extends Block {
       value: '',
       required: false
     });
-    // this.list = document.createElement(this.getElement('list'));
-    // let element = document.createElement('')
-    // this.showList = this.showList.bind(this);
-    // this.hideList = this.hideList.bind(this);
   }
-  // showList (event) {
-  //   let listRect = event.target.getBoundingClientRect();
-  //   // let elRect = this.helper.getBoundingClientRect();
-  //   helper.style.left = `${listRect.left + 10}px`
-  //   helper.style.top = `${listRect.bottom + 10}px`;
-  //   // console.log(list);
-  //   let list = document.createElement('ul');
-  //   let email1 = document.createElement('li');
-  //   let email2 = document.createElement('li');
-  //   email1.innerHTML = 'alexey.khabarov@mail.ru';
-  //   email2.innerHTML = 'example@example.ru';
-  //   list.appendChild(email1);
-  //   list.appendChild(email2);
-  //   helper.appendChild(list);
-  //   // console.log(email.querySelector(`.${this.bemName}`));
-  //   this.email.querySelector(`.${this.bemName}__email`).appendChild(this.helper);
-  //   // helper.style.display = helper.style.display ? '' : 'none';
-  // }
-  // hideList () {
-  //   // helper.style.display = 'none';
-  //   console.log('call hideList');
-  // }
   render (el) {
     super.render(el);
     this.email.render(this.getElement('email'));
-    // this.list.render(this.getElement('list'));
-    // console.log('call render');
-    // this.el.querySelector('form').addEventListener('submit', event => {
-    //   event.preventDefault();
-    //   console.log(this.email);
-    // })
-    // this.el.querySelector('input').addEventListener('click', this.showList);
+    // this.el.querySelector('input').addEventListener('click', event => {
+    //   let className = `${this.bemName}__list`;
+    //   this.el.querySelector(`.${className}`).classList.toggle(`${className}_active`);
+    // });
+    this.el.querySelector('form').addEventListener('click', event => {
+      let className = `${this.bemName}__list`;
+      if (event.target === this.el.querySelector('input')) {
+        this.el.querySelector(`.${className}`).classList.toggle(`${className}_active`);
+      } else {
+        this.el.querySelector(`.${className}`).classList.remove(`${className}_active`);
+      }
+    });
   }
 }
